fbf54882246b527ce20a791c53946f43d455db8d
Mariana018 <ortegamariana483@gmail.com>
2023-03-02 11:50:40 -0600

    "Tarea"

--- Trabajo.R
+++ Trabajo.R
@@ -18,3 +18,42 @@ mean(Viveros$IE)
 prof_url <- "http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionfoanp.csv"
 profepa <- read.csv(prof_url)
 
+
+summary(profepa)
+profepa$Operativo == max(profepa$Operativo)
+
+prof.soc <- subset(profepa, profepa$Operativo != max(profepa$Operativo))
+
+boxplot(prof.soc$Operativo)
+boxplot(prof.soc$Recorrido)
+prof_url2 <- paste0("http://www.profepa.gob.mx/innovaportal/","file/7635/1/accionesInspeccionfoanp.csv")
+profepa2 <- read.csv(prof_url2)
+summary(profepa2)
+summary(profepa)
+
+library(repmis)
+
+conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
+
+
+boxplot(diametro)
+boxplot(conjunto$Diametro)
+
+
+conjunto$Especie <- as.factor(conjunto$Especie)
+conjunto$Clase <- as.factor(conjunto$Clase)
+boxplot(conjunto$Diametro - conjunto$Especie)
+boxplot(conjunto$Diametro ~ conjunto$Especie,
+        col="lightgreen",
+        xlab ="Especie",
+        ylab ="Diametro",
+        main="Bosque obscuro")
+
+
+
+boxplot(conjunto$Altura ~ conjunto$Especie,
+        col="lightgreen",
+        xlab ="Especie",
+        ylab ="Diametro",
+        main="Bosque obscuro")
+
