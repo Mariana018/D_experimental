80b1a9c89c0e500dd4678ff3bc5488c1221d5809
Mariana018 <ortegamariana483@gmail.com>
2023-03-02 08:47:32 -0600

    "TRABAJO01"

---
+++ Trabajo.R
@@ -0,0 +1,16 @@
+#Laboratorio2
+#2017439_Mariana
+
+#01/03/2023
+
+# funcion de read.csv
+
+Viveros <- read.csv("Scripts/BD1_Viveros.csv", header = TRUE)
+# CONVERTIR TRATAMIENTO A FACTOR
+
+Viveros$Tratamiento <- as.factor(Viveros$Tratamiento)
+
+summary(Viveros)
+sd(Viveros$IE)
+var(Viveros$IE)
+mean(Viveros$IE)
