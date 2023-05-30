26acf8159aede635d0bd94aba6cfa0b6ea5d17d8
Mariana018 <124898043+Mariana018@users.noreply.github.com>
2023-05-30 11:57:14 -0600

    Add files via upload
    Tareas_Mariana

---
+++ Tareas/hw3.R
@@ -0,0 +1,70 @@
+##Yaretzy Mariana Ortega Martinez 
+#2017439
+##22/03/2023
+
+#HW3
+#Problema 1
+
+∑ 4 (yo=1)= xi 
+
+#Problema 2
+
+grupo_a <-c(80, 90, 90, 100)
+grupo_b <-c(60, 65, 65, 70, 70, 70, 75, 75, 80, 80, 80, 80, 80, 85, 100 )
+## como altura media mayor la tiene el grupo a ya que no existen tantos datos
+##Media del grupo A
+
+mean(grupo_a)
+mean(grupo_b)
+#el grupo A tiene la media mayor 
+#si 
+
+#Problema 3
+
+valoraciones <- c(87, 72, 85, 79)
+signif(valoraciones)
+##Necesita una calificacion de 79
+
+#Problema 4
+##Respuesta (A)
+
+#Probelma 5
+
+germinaciones <- read.csv( "Scripts/germinaciones.csv", header = TRUE)
+
+##Se puede utilizar un grafico de barras
+#B medios 
+mean(germinaciones $ germinaciones)
+
+## C mediana
+
+median(germinaciones $ germinaciones)
+
+
+#Problema 6
+
+conjunto <- c(2, 2, 3, 6, 10)
+signif(conjunto)
+median(conjunto)
+
+
+##crear funcion getmoda
+
+getmoda <- funcion (v) {uniqv <- unico (v) uniqv [cual.max(tabular(coincidencia(v, uniqv)))]}
+
+
+## +5
+conjunto_1 <- c(7, 7, 8, 11, 15)
+mean(conjunto_1)
+median(conjunto_1)
+
+##c
+## similitud en los datos 
+
+##D
+
+conjunto_2 <- c(10, 10, 15, 30, 50)
+mean(conjunto_2)
+median(conjunto_2)
+
+##Rango mayor
